6d628f9101b0b722aa3a87af72f3949b262892e7
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-08-08 10:41:17 -0300

    the script at least runs. Lets write the spec for all the instructions now :-)

--- anotherworld/microcode.py
+++ anotherworld/microcode.py
@@ -7,7 +7,7 @@ SIGNALS = \
 {
   "ALU_CARRY_IN": {"bits": 1, "shift": 0},
   "ALU_MODE": {"bits": 1, "shift": 1},
-  "ALU_FUNCTION": {"bits": 4, "shift": 2},
+  "ALU_FUNCTION_SEL": {"bits": 4, "shift": 2},
   "ALU_IN_AL_CLK": {"bits": 1, "shift": 6},
   "ALU_IN_AH_CLK": {"bits": 1, "shift": 7},
   "ALU_IN_BL_CLK": {"bits": 1, "shift": 8},
@@ -18,8 +18,9 @@ SIGNALS = \
   "OPCODE_CLK": {"bits": 1, "shift": 13},
   "~SRAM_WE": {"bits": 1, "shift": 14},
   "DATABUS_SEL": {"bits": 3, "shift": 15},
-  "SRAM_RANGE_SEL": {"bits": 2, "shift": 18}
-  "~CUR_CHAN_OE": {"bits": 1, "shift": 20}
+  "SRAM_RANGE_SEL": {"bits": 2, "shift": 18},
+  "~CUR_CHAN_OE": {"bits": 1, "shift": 20},
+  "SELECT_BYTE": {"bits": 1, "shift": 21}
 }
 
 # Databus selectors:
@@ -48,6 +49,9 @@ LOGIC = 1
 #ALU arithmetic functions:
 INC_A = 0xF
 
+# helper define:
+HIGH = 1
+LOW = 0
 
 FETCH_OPCODE = \
 [
@@ -62,7 +66,7 @@ FETCH_OPCODE = \
   ("ALU_FUNCTION_SEL", INC_A),
       ("ALU_CARRY_IN", HIGH)
  ],
- [    ("CHANNEL_BYTE", LOW_BYTE),
+ [     ("SELECT_BYTE", LOW_BYTE),
            ("IPH_CLK", LOW),
      ("ALU_IN_AH_CLK", LOW)
  ],
@@ -140,6 +144,9 @@ for addr in xrange(2**num_inputs):
   output = 0
   opcode_value = (addr >> 0) & 255
   cycle = (addr >> 8) & 31
+
+  if opcode_value > 6:
+    continue  # we havent yet specified these instructions
 
   if cycle < len(OPCODE[opcode_value]):
     for signal, value in OPCODE[opcode_value][cycle]:
